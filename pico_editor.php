071a9e8606eb77e29d32370f8004447b6fec2212
Alexandre Kaspar <xion.luhnis@gmail.com>
2014-07-14 20:04:06 +0200

    Reformatting and commenting a bit in the code

--- pico_editor.php
+++ pico_editor.php
@@ -30,11 +30,24 @@ class Pico_Editor {
     }
   }
 
+  /**
+   * Access a setting
+   *
+   * @param $name string the setting name
+   * @param $defaultValue varying value to return if no key is found
+   * @return the value of the named setting or FALSE
+   */
   protected function setting($name, $defaultValue = FALSE) {
     if(array_key_exists($name, $this->settings)) return $this->settings[$name];
     return $defaultValue;
   }
 
+  /**
+   * Hook: request mapping into actions
+   * No twig rendering is made here.
+   *
+   * @param $url string the current url to respond for
+   */
   public function request_url(&$url) {
     // Are we looking for /admin?
     if($url == 'admin') $this->is_admin = true;
@@ -45,6 +58,12 @@ class Pico_Editor {
     if($url == 'admin/logout') $this->is_logout = true;
   }
 
+  /**
+   * Hook: before rendering with Twig
+   *
+   * @param $twig_vars array twig environment variables
+   * @param $twig Twig instance of twig
+   */
   public function before_render(&$twig_vars, &$twig) {
     if($this->is_logout){
       session_destroy();
@@ -90,129 +109,149 @@ class Pico_Editor {
    * @return string
    */
   private static function get_real_filename($file_url) {
-
     $file_components = parse_url($file_url); // inner
     $base_components = parse_url($_SESSION['pico_config']['base_url']);
     $file_path = rtrim($file_components['path'], '/');
     $base_path = rtrim($base_components['path'], '/');
 
-    if(empty($file_path) || $file_path === $base_path)
-        {
-            return 'index';
-    }
-        else
-        {
-            $file_path = strip_tags(substr($file_path, strlen($base_path)));
-            if(is_dir(CONTENT_DIR . $file_path))
-                $file_path .= "/index";
+    if(empty($file_path) || $file_path === $base_path) {
+      return 'index';
+    } else {
+      $file_path = strip_tags(substr($file_path, strlen($base_path)));
+      if(is_dir(CONTENT_DIR . $file_path))
+        $file_path .= "/index";
 
-            return $file_path;
-        }
+      return $file_path;
+    }
   }
 
+  /**
+   * Transform text into a slug version for file names
+   *
+   * @param $text string the string to filter
+   * @return the filtered text
+   */
+  private static function slugify($text) {
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+    // trim
+    $text = trim($text, '-');
+    // transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // lowercase
+    $text = strtolower($text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
 
+    if (empty($text)){
+      return 'n-a';
+    }
+    return $text;
+  }
+
+  /**
+   * Check whether login is done
+   * or die as not authorized to go farther
+   */
+  private function check_login() {
+    if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']){
+      die(json_encode(array('error' => 'Error: Unauthorized')));
+    }
+  }
+
+  //
+  // Create a new page ////////////////////////////////////////////////////////
+  //
   private function do_new()  {
-        if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unauthorized')));
-        $title = isset($_POST['title']) && $_POST['title'] ? strip_tags($_POST['title']) : '';
-        $dir = isset($_POST['dir']) && $_POST['dir'] ? strip_tags($_POST['dir']) : '';
+    $this->check_login();
+    $title = isset($_POST['title']) && $_POST['title'] ? strip_tags($_POST['title']) : '';
+    $dir = isset($_POST['dir']) && $_POST['dir'] ? strip_tags($_POST['dir']) : '';
 
-        $contentDir = CONTENT_DIR . $dir;
-        if($contentDir[strlen(count($contentDir)-1)] != '/') $contentDir .= '/';
+    $contentDir = CONTENT_DIR . $dir;
+    if($contentDir[strlen(count($contentDir)-1)] != '/') $contentDir .= '/';
 
-        if(!is_dir($contentDir)) {
-            if (!mkdir($contentDir, 0777, true)) {
-                die(json_encode(array('error' => 'Can\'t create directory...')));
-            }
-        }
-
-        $file = $this->slugify(basename($title));
-        if(!$file) die(json_encode(array('error' => 'Error: Invalid file name')));
-
-        $error = '';
-        $file .= CONTENT_EXT;
-        $content = '/*
-Title: '. $title .'
-Author:
-Date: '. date('Y/m/d') .'
-*/';
-        if(file_exists($contentDir . $file))
-        {
-            $error = 'Error: A post already exists with this title';
-        }
-        else
-        {
-            if(strlen($content) !== file_put_contents($contentDir . $file, $content))
-                $error = 'Error: can not create the post ... ';
-        }
-
-        $file_url = $dir .'/'. str_replace(CONTENT_EXT, '', $file);
-
-        die(json_encode(array(
-            'title' => $title,
-            'content' => $content,
-            'file' => $file_url,
-            'error' => $error
-        )));
+    if(!is_dir($contentDir)) {
+      if (!mkdir($contentDir, 0777, true)) {
+        die(json_encode(array('error' => 'Can\'t create directory...')));
+      }
     }
 
-    private function do_open() {
-        if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unauthorized')));
-        $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
-        $file = self::get_real_filename($file_url);
-        if(!$file) die('Error: Invalid file');
+    $file = $this->slugify(basename($title));
+    if(!$file) die(json_encode(array('error' => 'Error: Invalid file name')));
 
-        $file .= CONTENT_EXT;
-        if(file_exists(CONTENT_DIR . $file)) die(file_get_contents(CONTENT_DIR . $file));
-        else die('Error: Invalid file');
+    $error = '';
+    $file .= CONTENT_EXT;
+    $content = '/*
+      Title: '. $title .'
+      Author:
+      Date: '. date('Y/m/d') .'
+     */';
+    if(file_exists($contentDir . $file)) {
+      $error = 'Error: A post already exists with this title';
+    } else {
+      if(strlen($content) !== file_put_contents($contentDir . $file, $content))
+        $error = 'Error: can not create the post ... ';
     }
 
-    private function do_save() {
-        if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unauthorized')));
-        $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
-        $file = self::get_real_filename($file_url);
-        if(!$file) die('Error: Invalid file');
-        $content = isset($_POST['content']) && $_POST['content'] ? $_POST['content'] : '';
-        if(!$content) die('Error: Invalid content');
+    $file_url = $dir .'/'. str_replace(CONTENT_EXT, '', $file);
 
-        $file .= CONTENT_EXT;
-        $error = '';
-        if(strlen($content) !== file_put_contents(CONTENT_DIR . $file, $content))
-            $error = 'Error: can not save changes ... ';
+    die(json_encode(array(
+      'title' => $title,
+      'content' => $content,
+      'file' => $file_url,
+      'error' => $error
+    )));
+  }
 
-        die(json_encode(array(
-            'content' => $content,
-            'file' => $file_url,
-            'error' => $error
-        )));
-    }
+  //
+  // Create a new page ////////////////////////////////////////////////////////
+  //
+  private function do_open() {
+    $this->check_login();
+    $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
+    $file = self::get_real_filename($file_url);
+    if(!$file) die('Error: Invalid file');
 
-    private function do_delete() {
-        if(!isset($_SESSION['pico_logged_in']) || !$_SESSION['pico_logged_in']) die(json_encode(array('error' => 'Error: Unauthorized')));
-        $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
-        $file = self::get_real_filename($file_url);
-        if(!$file) die('Error: Invalid file');
+    $file .= CONTENT_EXT;
+    if(file_exists(CONTENT_DIR . $file)) die(file_get_contents(CONTENT_DIR . $file));
+    else die('Error: Invalid file');
+  }
 
-        $file .= CONTENT_EXT;
-        if(file_exists(CONTENT_DIR . $file)) die(unlink(CONTENT_DIR . $file));
-    }
+  //
+  // Save a page //////////////////////////////////////////////////////////////
+  //
+  private function do_save() {
+    $this->check_login();
+    $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
+    $file = self::get_real_filename($file_url);
+    if(!$file) die('Error: Invalid file');
+    $content = isset($_POST['content']) && $_POST['content'] ? $_POST['content'] : '';
+    if(!$content) die('Error: Invalid content');
 
-    private function slugify($text) {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-        // trim
-        $text = trim($text, '-');
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        // lowercase
-        $text = strtolower($text);
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
+    $file .= CONTENT_EXT;
+    $error = '';
+    if(strlen($content) !== file_put_contents(CONTENT_DIR . $file, $content))
+      $error = 'Error: can not save changes ... ';
 
-        if (empty($text))
-            {
-                return 'n-a';
-            }
-        return $text;
-    }
+    die(json_encode(array(
+      'content' => $content,
+      'file' => $file_url,
+      'error' => $error
+    )));
+  }
+
+  //
+  // Remove a page ////////////////////////////////////////////////////////////
+  //
+  private function do_delete() {
+    $this->check_login();
+    $file_url = isset($_POST['file']) && $_POST['file'] ? $_POST['file'] : '';
+    $file = self::get_real_filename($file_url);
+    if(!$file) die('Error: Invalid file');
+
+    $file .= CONTENT_EXT;
+    if(file_exists(CONTENT_DIR . $file)) die(unlink(CONTENT_DIR . $file));
+  }
+
 }
 ?>
